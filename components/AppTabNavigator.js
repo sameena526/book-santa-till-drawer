82b9854d9794dbe7eb7d275fcbef6eca0dd69dea
sameena526 <75252582+sameena526@users.noreply.github.com>
2021-05-12 13:11:03 +0530

    Add files via upload

---
+++ components/AppTabNavigator.js
@@ -0,0 +1,18 @@
+   import React from 'react';
+   import { Image } from 'react-native';
+   import { createBottomTabNavigator } from 'react-navigation-tabs';
+   import BookDonateScreen from '../screens/BookDonateScreen';
+   import BookRequestScreen from '../screens/BookRequestScreen';
+
+
+
+   export const AppTabNavigator = createBottomTabNavigator({
+       DonateBooks: {
+           screen: BookDonateScreen,
+
+       },
+       BookRequest: {
+           screen: BookRequestScreen,
+
+       }
+   });
